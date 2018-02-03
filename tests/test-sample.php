e319fb374f9703ae62249f3fb4121752bab60f8d
Klaus Harris <klasharr42@gmail.com>
2018-02-03 13:13:35 -0200

    Checking Unit testing is set properly, it is.

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -14,7 +14,13 @@ class SampleTest extends WP_UnitTestCase {
 	 * A single example test.
 	 */
 	function test_sample() {
+
+		wp_set_current_user( 1,'admin');
+
 		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$this->assertTrue( is_user_logged_in() );
 	}
+
+
+
 }
